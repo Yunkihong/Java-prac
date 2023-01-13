884a6e7fcebdb707ac0401adc5574750502f3779
Yunkihong <81303136+Yunkihong@users.noreply.github.com>
2023-01-13 15:03:17 +0900

    Add files via upload

--- Sign_in1.java
+++ Sign_in1.java
@@ -5,17 +5,17 @@ class Sign_in {
 	int year, month, day;
 	String id;
 	String pw;
-	String sex;
+	String manwomen;
 	String phone;
 
-	Sign_in(String name, int year, int month, int day, String id, String pw, String sex, String phone) {
+	Sign_in(String name, int year, int month, int day, String id, String pw, String manwomen, String phone) {
 		this.name = name;
 		this.year = year;
 		this.month = month;
 		this.day = day;
 		this.id = id;
 		this.pw = pw;
-		this.sex = sex;
+		this.manwomen = manwomen;
 		this.phone = phone;
 	}
 
@@ -35,10 +35,8 @@ class Sign_in {
 }
 
 public class Sign_in1 {
-	public static void main(String[] args) {
-		
-		Sign_in si1 = new Sign_in("홍윤기eeee", 2000, 01, 30, "ricky0130", "알빠노", "남자", "01096648141");
-		
+	public static void main(String[] args) {   
+		Sign_in si1 = new Sign_in("홍윤기", 2000, 01, 30, "ricky0130", "알빠노", "남자", "01096648141");
 		System.out.println(si1.hidePw(si1.pw));
 
 	}
