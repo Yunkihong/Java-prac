638a111f05208992dc5a5ad471d3e1d188e86f50
Yunkihong <81303136+Yunkihong@users.noreply.github.com>
2023-01-17 01:57:07 +0900

    Add files via upload

--- Sign_in1.java
+++ Sign_in1.java
@@ -31,23 +31,23 @@ class Sign_in {
 
 		return firstname;
 	}
-	// 첫번째 빼고 나머지 비밀번호 전부를 가리는 코드
-//	String hidePw(String pw) {
-//		String hidpw = "", bin = "*************************************************";
-//		char c = 0;
-//		for (int i = 0; i < pw.length(); i++) {
-//			c = pw.charAt(i + 1);
-//			c = bin.charAt(i);
-//			hidpw += c;
-//		}
-//		return hidpw;
-//	}
+//	 첫번째 빼고 나머지 비밀번호 전부를 가리는 코드
+	String hidePw(String pw) {
+		String hidpw = "", bin = "*************************************************";
+		char c = 0;
+		for (int i = 0; i < pw.length(); i++) {
+			c = pw.charAt(i + 1);
+			c = bin.charAt(i);
+			hidpw += c;
+		}
+		return hidpw;
+	}
 }
 
 public class Sign_in1 {
 	public static void main(String[] args) {
 		Sign_in si1 = new Sign_in("홍윤기", 2000, 01, 30, "ricky0130", "알빠노", "남자", "01096648141");
-//		System.out.println(si1.hidePw(si1.pw));
+		System.out.println(si1.hidePw(si1.pw));
 		System.out.println(si1.firstLastname(si1.name));
 		System.out.println(si1.lastname);
 		System.err.println(si1.firstname);
